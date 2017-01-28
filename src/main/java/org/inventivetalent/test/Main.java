33c45e19fb4527a7c45b55cf42a8380f22f301c8
inventivetalent <inventivetalent@inventivegames.de>
2017-01-28 18:38:43 +0100

    Initial commit

---
+++ src/main/java/org/inventivetalent/test/Main.java
@@ -0,0 +1,28 @@
+package org.inventivetalent.test;
+
+/**
+ * Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Nulla ultricies imperdiet elementum. Morbi non fringilla orci.
+ * Curabitur nulla mi, feugiat ac sodales ut, auctor a diam.
+ * Phasellus sagittis dapibus aliquet. Praesent pulvinar metus nec libero consequat mattis.
+ * Sed vel pellentesque enim. In molestie finibus risus in sollicitudin.
+ */
+public class Main {
+
+	/**
+	 * Aenean vel lacus justo.
+	 * Nam sodales erat a augue euismod, ut porttitor nisi maximus.
+	 * Nullam et nibh ullamcorper, sollicitudin augue id, semper neque.
+	 * Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent tellus tellus, vestibulum eget dui venenatis, fringilla gravida libero.
+	 * Sed leo lorem, consequat at tortor vitae, elementum malesuada lectus.
+	 * Nulla nisl ante, porttitor nec consectetur sit amet, fringilla id turpis.
+	 * Nunc eu pulvinar mi. In dolor augue, blandit hendrerit consequat in, gravida at augue.
+	 * Maecenas a augue pharetra, sodales est ut, dignissim est. Nam viverra lacus ac commodo maximus.
+	 * Donec ac nisi eu est pretium cursus. Etiam ac metus nunc. Morbi sit amet purus a libero ultrices dignissim ac non nisl.
+	 *
+	 * @param args Whatever
+	 */
+	public static void main(String[] args) {
+	}
+
+}
